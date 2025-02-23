919011895d865d12099ca96f751a1370506467ea
Ayesha Khan <silentprogrammer0001@gmail.com>
2025-02-23 11:48:25 +0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,10 @@
+print("Hello World")# this line print a string
+# print*("Ayesha")
+print("Hello Time")
+print(3+4)# this line will print a number with + function and present the operator
+#variables
+x = 3+6+9*6
+print(x)
+x+4
+x+5
+x+6
